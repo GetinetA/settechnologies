4f99633983a6fc4fa98712c445be46ce36ca5600
Getinet Alewab <getinetbr@yahoo.com>
2018-12-02 07:06:05 -0500

    Update script.js
    increase slider delay

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ $(document).ready(function() {
 		animation: "fade",
 		directionNav: false,
 		pauseOnAction: false,
-		slideshowSpeed: 7000,
+		slideshowSpeed: 15000,
 	});
 
 	var containerPosition = $('.container').offset();
